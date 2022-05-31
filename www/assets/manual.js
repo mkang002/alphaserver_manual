6e2dde999e08fbbcae69fface28df8eecdf810e8
Jitka Černohorská <cernojit@gmail.com>
2022-05-31 12:59:29 +0200

    feat(first-itertion): iteration 1.0

---
+++ www/assets/manual.js
@@ -0,0 +1,67 @@
+const initDraggable = () => {
+  dragElement(document.getElementById("manual"))
+
+  function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0
+    const element = document.getElementById("draggable")
+
+    if (element) {
+      element.onmousedown = dragMouseDown;}
+    else {elmnt.onmousedown = dragMouseDown;}
+
+    function dragMouseDown(w) {
+      w = w || window.event;
+      w.preventDefault();
+      pos3 = w.clientX;
+      pos4 = w.clientY;
+      window.onmouseup = closeDragElement;
+      window.onmousemove = elementDrag;}
+
+    function elementDrag(w) {
+      w = w || window.event;
+      w.preventDefault();
+      pos1 = pos3 - w.clientX;
+      pos2 = pos4 - w.clientY;
+      pos3 = w.clientX;
+      pos4 = w.clientY;
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";}
+
+    function closeDragElement() {
+      window.onmouseup = null
+      window.onmousemove = null
+    }
+  }
+}
+
+function initToggleManual() {
+  const closeBtn = document.getElementById("close-btn")
+  const openBtn = document.getElementById("info-btn")
+  closeBtn.onclick = () => {
+    document.getElementById("manual").style.display = "none"
+    document.getElementById("info-btn").style.display = "block"
+  }
+  openBtn.onclick = () => {
+    document.getElementById("manual").style.display = "block"
+    document.getElementById("info-btn").style.display = "none"
+  }
+}
+
+
+window.onload = function(){
+
+  const hash = window.location.hash
+  const pageId = hash.split('/')
+  document.getElementById("manual-text").setAttribute("src", `./assets/manual/${pageId[2] ?? 'main'}.html`);
+
+  initDraggable()
+  initToggleManual()
+
+  // change embed source
+  window.addEventListener('popstate', function() {
+    const hash = window.location.hash
+    const pageId = hash.split('/')
+    document.getElementById("manual-text").setAttribute("src", `./assets/manual/${pageId[2] ?? 'main'}.html`);
+  });
+}
+
